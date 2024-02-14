bebc11767aa78df2d3720181dd6b3a7c0ed5eb43
Prithwish Chakraborty <prithwishchakaborty05@gmail.com>
2024-02-14 10:38:18 +0530

    step1

---
+++ src/Foot.jsx
@@ -0,0 +1,9 @@
+function Footing(){
+    return(
+        <footer>
+            <p>&copy;{new Date().getFullYear()}PRITHWISH </p>
+        </footer>
+    )
+}
+
+export default Footing
---
+++ src/Header.jsx
@@ -0,0 +1,19 @@
+function Header() {
+    return(
+        <header>
+            <h1>yo whats up dudes</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">HOME</a></li>
+                    <li><a href="#">SPECIAL</a></li>
+                    <li><a href="#">EXTREAM</a></li>
+                    <li><a href="#">CENTIMEMTAL</a></li>
+                    <li><a href="#">PROFILE</a></li>
+                    <li><a href="#">HELP</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
